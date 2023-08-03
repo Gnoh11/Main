b9e499ab3acdf0a57a41877745d54e08147b485d
7AM <89345506+Gnoh11@users.noreply.github.com>
2023-08-03 23:19:06 +0700

    Add files via upload

---
+++ Game List.lua
@@ -0,0 +1,19 @@
+getgenv().Games = {
+    -- Arm Wrestle Simulator
+    [13127800756] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Arm%20Wrestle%20Simulator.lua",
+    -- Anime Fighters Simulator
+    [6299805723] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Fighter%20Simulator.lua",
+
+
+}
+
+getgenv().Unknown = {
+    -- Shinobi Life 2
+    "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Shindo%20Life",
+    -- Pixel Piece
+    "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Pixel%20Piece.lua",
+    -- Pet Simulator X
+    "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Pet%20Simulator%20X.lua",
+    -- Doors
+    "https://raw.githubusercontent.com/Gnoh11/Game-List/main/DOORS.lua",
+}
