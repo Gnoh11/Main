a2a2ad372365ba68d51233ccab103f6a8babb8a6
7AM <89345506+Gnoh11@users.noreply.github.com>
2023-08-06 21:32:05 +0700

    Add files via upload

--- Game List.lua
+++ Game List.lua
@@ -1,9 +1,78 @@
 getgenv().Games = {
+    -- +1 Blocks Every Second
+    [11800876530] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/%2B1%20Blocks%20Every%20Second.lua",
+    -- +1 Jump Every Second
+    [9834528893] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/%2B1%20Jump%20Every%20Second.lua",
+    -- +1 Per Second
+    [11929558254] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/%2B1%20Per%20Second.lua",
+    -- Adopt Me!
+    [920587237] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Adopt%20Me.lua",
+    --Anime Adventures
+    [8304191830] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Adventures.lua",
+    -- Anime Fruit Simulator
+    [10446125875] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Fruit%20Simulator.lua",
+    -- Anime Souls Simulator
+    [11542692507] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Souls%20Simulator.lua",
+    -- Anime Weapon Simulator
+    [11430505281] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Weapon%20Simulator.lua",
+    -- Bee Swarm Simulator
+    [1537690962] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Bee-Swarm-Simulator",
+    -- Blox Fruits
+    [2753915549] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Blox%20Fruits.lua",
+    -- Brookhaven RP
+    [4924922222] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Brookhaven%20RP.lua",
+    -- Build A Boat For Treasure
+    [537413528] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Build%20A%20Boat%20For%20Treasure.lua",
+    -- Car Dealership Tycoon
+    [1554960397] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Car%20Dealership%20Tycoon.lua",
+    -- Car Factory Tycoon!
+    [11874473440] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Car%20Factory%20Tycoon.lua",
+    -- Clicker Simulator
+    [7560156054] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Clicker%20Simulator.lua",
+    -- Combat Warriors
+    [4282985734] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Combat%20Warriors.lua",
+    -- Da Hood
+    [2788229376] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Da%20Hood.lua",
+    -- Deepwoken
+    [4111023553] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Deepwoken.lua",
+    -- Driving Empire
+    [3351674303] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Driving%20Empire.lua",
+    -- Every Second You Get +1 Damage
+    [11495558803] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Every%20Second%20You%20Get%20%2B1%20Damage.lua",
+    -- Every Second You Get +1 Health
+    [11302865934] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Every%20Second%20You%20Get%20%2B1%20Health.lua",
+    -- Every Second You Get +1 Jump Power
+    [11063612131] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Every%20Second%20You%20Get%20%2B1%20Jump%20Power.lua",
+    -- Fly Race!
+    [6679968919] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Fly%20Race!.lua",
+    -- Jailbreak
+    [606849621] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Jailbreak.lua",
+    -- King Legacy
+    [4520749081] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/King%20Legacy.lua",
+    -- Mad City
+    [1224212277] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Mad%20City.lua",
+    -- Military Tycoon
+    [7180042682] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Military%20Tycoon.lua",
+    --Murder Mystery 2
+    [142823291] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Murder%20Mystery%202.lua",
+    -- Pickaxe Mining Simulator
+    [10821317529] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Pickaxe%20Mining%20Simulator.lua",
+    -- Race Clicker
+    [9285238704] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Race%20Clicker.lua",
+    -- Smoothie Factory Tycoon
+    [10905034443] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Smoothie%20Factory%20Tycoon.lua",
+    -- Super Hero Tycoon
+    [574407221] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Super%20Hero%20Tycoon.lua",
+    -- Super Power Fighting Simulator
+    [2619187362] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Super%20Power%20Fighting%20Simulator.lua",
+    -- Sword Fighters Simulator
+    [11040063484] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Sword-Fighters-Simulator",
+    -- The Survival Game
+    [11156779721] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/The%20Survival%20Game.lua",
+    -- Ultra Power Tycoon
+    [8146731988] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Ultra%20Power%20Tycoon.lua",
     -- Arm Wrestle Simulator
     [13127800756] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Arm%20Wrestle%20Simulator.lua",
-    -- Anime Fighters Simulator
-    [6299805723] = "https://raw.githubusercontent.com/Gnoh11/Game-List/main/Anime%20Fighter%20Simulator.lua"
-
 
 }
 
